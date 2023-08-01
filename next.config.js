9d75fd47e181285b139e3800c5cf28fa84635a4e
Carwi Barrios <carwelsbarrios@gmail.com>
2023-08-01 15:23:04 +0200

    first commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  sassOptions: {
+    prependData: `
+    @import "src/assets/scss/abstracts/_variables.scss";
+    @import "src/assets/scss/abstracts/_mixins.scss";`,
+  },
 }
 
-module.exports = nextConfig
+module.exports = nextConfig;
