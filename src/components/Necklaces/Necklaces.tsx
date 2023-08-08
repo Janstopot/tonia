c43e24d74f9f31ac06ee631fdde9697d101fcb59
Alejandro Martinez <alejandro.mzrz@gmail.com>
2023-08-08 22:15:57 +0200

    press, contact y necklaces

--- src/components/Necklaces/Necklaces.tsx
+++ src/components/Necklaces/Necklaces.tsx
@@ -1,21 +1,23 @@
-import React from 'react'
+import React from "react";
 
-import styles from './Necklaces.module.scss'
+import styles from "./Necklaces.module.scss";
+
+import Image from "next/image";
+import woman from "../../assets/images/woman.png";
+import man from "../../assets/images/man.png";
+import Link from "next/link";
 
 function Necklaces() {
   return (
     <div className={styles.container}>
-        <div>NECKLACES</div>
-        <div className={styles.imageContainer}>
-            <div className={styles.imageLeft}>
-                <div>WOMAN</div>
-            </div>
-            <div className={styles.imageRight}>
-                <div>MAN</div>
-            </div>
-        </div>
+      <Link href="/">
+        <Image src={woman} alt="Woman" className={styles.image} />
+      </Link>
+      <Link href="/">
+        <Image src={man} alt="Man" className={styles.image} />
+      </Link>
     </div>
-  )
+  );
 }
 
-export default Necklaces
+export default Necklaces;
