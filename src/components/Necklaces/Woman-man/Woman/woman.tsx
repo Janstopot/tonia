91e29006e1b659204695c4c855c2bb8802bcd1e6
Alejandro Martinez <alejandro.mzrz@gmail.com>
2023-08-14 15:58:05 +0200

    galeria necklaces

--- src/components/Necklaces/Woman-man/Woman/woman.tsx
+++ src/components/Necklaces/Woman-man/Woman/woman.tsx
@@ -1,33 +1,61 @@
 import React from "react";
 import styles from "./Woman.module.scss";
 import Image from "next/image";
+import Link from "next/link";
 //import woman1 from "../../../../assets/images/woman-necklances/1.png";
 //import woman2 from "../../../../assets/images/woman-necklances/2.png"
 import woman1 from "../../../../assets/images/woman-necklances/womannecklace1.jpg";
-import woman2 from "../../../../assets/images/woman-necklances/womannecklace2.jpg"
+import woman2 from "../../../../assets/images/woman-necklances/womannecklace2.jpg";
 
 function Woman() {
   return (
     <div className={styles.main}>
       <div className={styles.box}>
 
-        <div className={styles.image}>
+        <Link href="/" className={styles.image}>
           <div className={styles.text}>ROMAN HEAD</div>
-          <link href="/" />
-          <Image src={woman1} alt="Woman1"/>
-        </div>
+          <Image src={woman1} alt="Woman1" />
+        </Link>
 
-        <div className={styles.image}>
-        <div className={styles.text}>ROMAN HEAD</div>
-          <link href="/"/>
-          <Image src={woman2} alt="Woman1"/>
-        </div>
+        <Link href="/" className={styles.image}>
+          <div className={styles.text}>ROMAN HEAD2</div>
+          <Image src={woman2} alt="Woman1" />
+        </Link>
 
-        
+        <Link href="/" className={styles.image}>
+          <div className={styles.text}>ROMAN HEAD</div>
+          <Image src={woman1} alt="Woman1" />
+        </Link>
 
+        <Link href="/" className={styles.image}>
+          <div className={styles.text}>ROMAN HEAD2</div>
+          <Image src={woman2} alt="Woman1" />
+        </Link>
 
-        
+        <Link href="/" className={styles.image}>
+          <div className={styles.text}>ROMAN HEAD</div>
+          <Image src={woman1} alt="Woman1" />
+        </Link>
 
+        <Link href="/" className={styles.image}>
+          <div className={styles.text}>ROMAN HEAD2</div>
+          <Image src={woman2} alt="Woman1" />
+        </Link>
+
+        <Link href="/" className={styles.image}>
+          <div className={styles.text}>ROMAN HEAD</div>
+          <Image src={woman1} alt="Woman1" />
+        </Link>
+
+        <Link href="/" className={styles.image}>
+          <div className={styles.text}>ROMAN HEAD2</div>
+          <Image src={woman2} alt="Woman1" />
+        </Link>
+
+        <Link href="/" className={styles.image}>
+          <div className={styles.text}>ROMAN HEAD</div>
+          <Image src={woman1} alt="Woman1" />
+        </Link>
 
       </div>
     </div>
