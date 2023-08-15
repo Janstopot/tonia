7019ff5ab4178ac4cdac4debc69f7123c9b9cf27
Alejandro Martinez <alejandro.mzrz@gmail.com>
2023-08-15 16:20:55 +0200

    exhibitions y título en necklaces

--- src/components/Necklaces/Necklaces.tsx
+++ src/components/Necklaces/Necklaces.tsx
@@ -12,17 +12,21 @@ import Link from "next/link";
 function Necklaces() {
   return (
     <div className={styles.main}>
-      <Link href="/woman" className={styles.image}>
-        <div className={styles.text}>WOMAN</div>
-        <Image src={woman} alt="Woman" />
-      </Link>
+      <div className={styles.title}>NECKLACES</div>
 
-      <div className={styles.separation}></div>
+      <div className={styles.box}>
+        <Link href="/woman" className={styles.image}>
+          <div className={styles.text}>WOMAN</div>
+          <Image src={woman} alt="Woman" />
+        </Link>
 
-      <Link href="/" className={styles.image}>
-        <div className={styles.text}>MAN</div>
-        <Image src={man} alt="Man" />
-      </Link>
+        <div className={styles.separation}></div>
+
+        <Link href="/" className={styles.image}>
+          <div className={styles.text}>MAN</div>
+          <Image src={man} alt="Man" />
+        </Link>
+      </div>
     </div>
   );
 }
