392608b14511787ff75aec8c8aaf9dc3cf4b6710
Carwi Barrios <carwelsbarrios@gmail.com>
2023-08-31 12:15:46 +0200

    Profile responsive added

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -13,19 +13,21 @@ function Profile() {
                     <div className={styles.title}>MEET THE ARTIST</div>
                     <div className={styles.content}>
                         <div className={styles.text}>
-                            <div className={styles.upperText}>
+                            <p>
                                 Born in France, in 1963 in Algeria where she will spend her adolescence, Tonia obtained a PhD in Public Health in the USA. Her work in this area has enabled her to practice and live throughout Africa, in countries as diverse as Mali, Zambia, Senegal, Cote d'Ivoire, Rwanda, Mamitania and others.
-                            </div>
-                            <div className={styles.lowerText}>
+                            </p>
+                            <p>
                                 She took advantage of these stays to collect pearls from Africa and to document their uses panni the tradithérapeutes and these some animist pretres. Now retired, Tonia devotes herself to her passion, the pearls of Africa.
-                            </div>
+                            </p>
                         </div>
                         <div className={styles.imageContainer}>
-                            <Image
-                                src={profile}
-                                alt="Profile picture"
-                                className={styles.profileImage}
-                            />
+                            <div className={styles.imageWrapper}>
+                                <Image
+                                    src={profile}
+                                    alt="Profile picture"
+                                    className={styles.profileImage}
+                                />
+                            </div>
                         </div>
                     </div>
                 </div>
