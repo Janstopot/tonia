05c5a8832a3984c9c37fdd35d1c57ad4aa57592c
Carwi Barrios <carwelsbarrios@gmail.com>
2023-08-31 19:53:39 +0200

    Console error fixes

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import Head from "next/head"
 import Navbar from "@/components/Navbar/Navbar"
 import Footer from '@/components/Footer/Footer';
 import '../styles/Home.module.css'
@@ -7,6 +8,9 @@ import '../styles/Home.module.css'
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <div className="app-container">
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Head>
       <Navbar />
       <div className="content">
         <Component {...pageProps} />
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,7 +5,6 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link rel="icon" href="/favicon.ico" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
