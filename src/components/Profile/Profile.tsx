3c2a43c695b2347d3d04349c73d472c36e9d13b1
Carwi Barrios <carwelsbarrios@gmail.com>
2023-09-18 19:11:38 +0200

    profile language added, view more button

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -30,28 +30,39 @@ function Profile() {
         setShowNewText(!showNewText);
     };
 
-    const descriptionText: Record<Language, string[]> = {
-        ENG: [
-            "✓ Conference on « Ritual Functions of Venetian Glass Beads in Africa » as part of the program for the yearly ‘Venice Glass Week’, Venice, Italy. September 2020",
-            "✓ Exhibit and conferences on « Powers of African Beads », National Bardo Museum, Museum of Prehistory and Ethnography, Algiers, Algeria, December 2014 to October 2015",
-            "✓ Conferences on ‘Functions of Beads’ at the Senegalese Superior Institute for Arts and Cultures, for Master students specialized in « Cultural Heritage ». Dakar, Senegal. 2010 and 2011",
-            "✓ Publications: *« The bead that gives his power to the Dogon Priest », Bulletin de la Society of Bead Researchers, autumn 2015. USA ; *« La Couleur de l'Invisible », 2010. Mali",
-            "✓ Membership in : * The ≪ Society of Bead Researchers ≫ (beadresearch.org); * The NGO PROMETRA (Promotion of Traditional Medicine), Dakar. This NGO regroups traditional healers of the 20 member countries (prometra.org)"
-        ],
-        FR: [
-            "✓ Conférence sur « Fonctions Rituelles des Perles de Venise en Afrique » dans le cadre de ‘La Semaine du Verre de Venise’, Venise, Italie, Septembre 2020",
-            "✓ Exposition et conférences sur « Pouvoirs des Perles d'Afrique », Musée National du Bardo, Musée de Préhistoire et d’Ethnographie, Alger, Algérie, Décembre 2014 à Octobre 2015",
-            "✓ Conférences à L’Institut Supérieur des Arts et des Cultures du Sénégal à des étudiants de Master 2 spécialisés en « Patrimoine Culturel ». Dakar, Sénégal. 2010 et 2011",
-            "✓ Publications: *« The bead that gives his power to the Dogon Priest », Bulletin de la Society of Bead Researchers, autumn 2015. USA ; *« La Couleur de l'Invisible », 2010. Mali",
-            "✓ Membre de la « Society of Bead Researchers » (beadresearch.org); et de l'ONG internationale PROMETRA (Promotion de la Médecine Traditionnelle), composée de tradithérapeutes de chacun des 17 pays africains membres de cette ONG (prometra.org)"
-        ]
+    const descriptionText: Record<Language, { title: string; texts: string[] }> = {
+        ENG: {
+            title: "DOCUMENTING THE FUNCTIONS OF AFRICAN BEADS",
+            texts: [
+                "✓ Conference on « Ritual Functions of Venetian Glass Beads in Africa » as part of the program for the yearly ‘Venice Glass Week’, Venice, Italy. September 2020",
+                "✓ Exhibit and conferences on « Powers of African Beads », National Bardo Museum, Museum of Prehistory and Ethnography, Algiers, Algeria, December 2014 to October 2015",
+                "✓ Conferences on ‘Functions of Beads’ at the Senegalese Superior Institute for Arts and Cultures, for Master students specialized in « Cultural Heritage ». Dakar, Senegal. 2010 and 2011",
+                "✓ Publications: *« The bead that gives his power to the Dogon Priest », Bulletin de la Society of Bead Researchers, autumn 2015. USA ; *« La Couleur de l'Invisible », 2010. Mali",
+                "✓ Membership in : * The ≪ Society of Bead Researchers ≫ (beadresearch.org); * The NGO PROMETRA (Promotion of Traditional Medicine), Dakar. This NGO regroups traditional healers of the 20 member countries (prometra.org)",
+            ],
+        },
+        FR: {
+            title: "RECHERCHES DE TONIA SUR LES FONCTIONS DES PERLES D'AFRIQUE",
+            texts: [
+                "✓ Conférence sur « Fonctions Rituelles des Perles de Venise en Afrique » dans le cadre de ‘La Semaine du Verre de Venise’, Venise, Italie, Septembre 2020",
+                "✓ Exposition et conférences sur « Pouvoirs des Perles d'Afrique », Musée National du Bardo, Musée de Préhistoire et d’Ethnographie, Alger, Algérie, Décembre 2014 à Octobre 2015",
+                "✓ Conférences à L’Institut Supérieur des Arts et des Cultures du Sénégal à des étudiants de Master 2 spécialisés en « Patrimoine Culturel ». Dakar, Sénégal. 2010 et 2011",
+                "✓ Publications: *« The bead that gives his power to the Dogon Priest », Bulletin de la Society of Bead Researchers, autumn 2015. USA ; *« La Couleur de l'Invisible », 2010. Mali",
+                "✓ Membre de la « Society of Bead Researchers » (beadresearch.org); et de l'ONG internationale PROMETRA (Promotion de la Médecine Traditionnelle), composée de tradithérapeutes de chacun des 17 pays africains membres de cette ONG (prometra.org)",
+            ],
+        },
     };
+
+    const titleText: Record<Language, string> = {
+        ENG: "MEET THE ARTIST",
+        FR: "RENCONTREZ L'ARTISTE",
+      };
 
     return (
         <>
             <div className={styles.main}>
                 <div className={styles.container}>
-                    <div className={styles.title}>MEET THE ARTIST</div>
+                    <div className={styles.title}>{titleText[currentLanguage as Language]}</div>
                     <div className={styles.content}>
                         <div className={styles.text}>
                             {!showNewText && (
@@ -63,13 +74,19 @@ function Profile() {
 
                             {showNewText && (
                                 <>
-                                    {descriptionText[currentLanguage as Language].map((text, index) => (
+                                    <div className={styles.textTitle}>
+                                        {descriptionText[currentLanguage as Language].title}
+                                    </div>
+                                    {descriptionText[currentLanguage as Language].texts.map((text, index) => (
                                         <p key={index}>{text}</p>
                                     ))}
                                 </>
                             )}
 
-                            <button className={styles.btn} onClick={toggleNewText}>
+                            <button
+                                className={`${styles.btn} ${showNewText ? styles.rotated : ''}`}
+                                onClick={toggleNewText}
+                            >
                                 {showNewText ? '' : ''}
                             </button>
                         </div>
