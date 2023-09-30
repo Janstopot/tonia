f31a55b5f3d85e19cac27209b46ded2fcd611a59
Alejandro Martinez <alejandro.mzrz@gmail.com>
2023-09-30 14:18:02 +0200

    custom scroll bar en los mixings

--- src/components/Navbar/Search/Search.tsx
+++ src/components/Navbar/Search/Search.tsx
@@ -1,15 +1,17 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./Search.module.scss";
 import { CSSTransition } from "react-transition-group";
 import { collection, getDocs, getFirestore, query } from "firebase/firestore";
 import { Necklace, Press } from "@/assets/interfaces";
-import { useCollectionDataOnce } from "react-firebase-hooks/firestore";
+import { useCollectionData, useCollectionDataOnce } from "react-firebase-hooks/firestore";
 import { BeatLoader } from "react-spinners";
 import exhibitionData from "@/components/Exhibitions/Information/exhibitionData";
+import Link from "next/link";
 
 function Search(props: any) {
   const [currentMenu, setCurrentMenu] = useState("main");
   const [menuHeight, setMenuHeight] = useState<number>(225);
+  const [dataArray, setDataArray] = useState<any[]>([])
 
   function calcHeight(el : HTMLElement){
     setMenuHeight(el.offsetHeight)
@@ -114,20 +116,22 @@ function Search(props: any) {
 
   function Press() {
     const [value, loading, error] = useCollectionDataOnce(collection(getFirestore(), "press"), {});
-    if (loading) return (<li><BeatLoader color="#777777" loading={loading} size={50} /></li>)
-
     const array: Press[] = value as Press[];
-    console.log(array)
-  
+    if (loading && !value) return (<li><BeatLoader color="#777777" loading={loading} size={50} /></li>)
     return (
+    <div>
       <div>
         <li>PRESS</li>
-          {array.map((press, key) => (
-            <li key={key}>{press.text}</li>
-          ))}
+        {array.map((press, key) => (
+          <Link className={styles.links} key={key} href={press.link} rel="noopener noreferrer" target="_blank">
+              <li >{press.text}</li>
+          </Link>
+        ))}
       </div>
+    </div>
     );
   }
+
 }
 
 export default Search;
